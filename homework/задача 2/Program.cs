e2dac1c023682a44be295cf2420efed94dbae49a
Anton <j10r2@mail.ru>
2022-08-07 20:20:42 +0300

    исправил ошибку для отрицательных чисел

--- homework/задача 2/Program.cs
+++ homework/задача 2/Program.cs
@@ -19,8 +19,10 @@ if (size > 10)
 }
 double [] arr;
 arr = new double [size];
-double max = 0;
-for (int i = 0; i < size; i++)
+Console.WriteLine("введите 1-е число");
+arr [0] = double.Parse(Console.ReadLine());
+double max = arr [0];
+for (int i = 1; i < size; i++)
 {
     Console.WriteLine("введите " + (i+1) + "-е число");
     arr [i] = double.Parse(Console.ReadLine());
