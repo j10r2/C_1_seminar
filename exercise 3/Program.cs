e4483e8d6282019e533140cceabea143f38331c5
Anton <j10r2@mail.ru>
2022-08-07 16:15:03 +0300

    exception processed

--- exercise 3/Program.cs
+++ exercise 3/Program.cs
@@ -1,40 +1,47 @@
 ﻿Console.WriteLine("Введите число от 1 до 7");
 int day = Convert.ToInt32(Console.ReadLine());
-switch (day)
+try
 {
-    case 1:
+    switch (day)
     {
-    Console.WriteLine("Понедельник");
-    break;
-    }
-    case 2:
+        case 1:
+        {
+        Console.WriteLine("Понедельник");
+        break;
+        }
+        case 2:
+        {
+        Console.WriteLine("Вторник");
+        break;
+        }
+        case 3:
+        {   
+        Console.WriteLine("Среда");
+        break;
+        }
+        case 4:
+        {
+        Console.WriteLine("Четверг");
+        break;
+        }
+        case 5:
+        {
+        Console.WriteLine("Пятница");
+        break;
+        }
+        case 6:
+        {
+        Console.WriteLine("Суббота");
+        break;
+        }
+        case 7:
+        {
+        Console.WriteLine("Воскресенье");
+        break;
+        }
+    }}
+    catch (Exception ex)
     {
-    Console.WriteLine("Вторник");
-    break;
+    Console.WriteLine();
     }
-    case 3:
-    {
-    Console.WriteLine("Среда");
-    break;
-    }
-    case 4:
-    {
-    Console.WriteLine("Четверг");
-    break;
-    }
-    case 5:
-    {
-    Console.WriteLine("Пятница");
-    break;
-    }
-    case 6:
-    {
-    Console.WriteLine("Суббота");
-    break;
-    }
-    case 7:
-    {
-    Console.WriteLine("Воскресенье");
-    break;
-    }
-}
+Console.WriteLine("Алё, гараж, от 1 до 7");
