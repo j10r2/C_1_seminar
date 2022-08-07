c73d3f80941c833d8abc91dd57f5a678d482a590
Anton <j10r2@mail.ru>
2022-08-07 20:05:56 +0300

    вывод чётных чисел от 1 до ...

---
+++ homework/задача 4/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите число");
+double a = double.Parse(Console.ReadLine());
+Console.WriteLine();
+if (a >= 2 | a <= 0)
+    {
+    if (a > 0)
+        {
+        for (int i = 2; i <= a; i+=2)
+            {
+            Console.Write(i + ", ");
+            }
+        }
+    else
+        {
+        for (int i = 0; i >= a; i-=2)
+            {
+            Console.Write(i + " ");
+            } 
+        }
+    }
+else
+    {
+        Console.WriteLine("Нет подходящих чисел для вывода");
+    }
