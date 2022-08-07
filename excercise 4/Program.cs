28a1c039cc7fee04a6620fb32190a031a504c5e2
Anton <j10r2@mail.ru>
2022-08-07 16:56:20 +0300

    array version

---
+++ excercise 4/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("введите число");
+
+try
+    {
+    double n = Convert.ToDouble(Console.ReadLine());
+    }
+catch
+    {
+    Console.WriteLine("ты читать не умеешь? Введи ЧИСЛО");
+    }
+ int m = Convert.ToInt32(Math.Round(n, 0));
+    // int [] arr;
+    // arr = new int [(m * 2) + 1];
+    // arr[0] = 1;
+    // arr[1] = 2;
+    // arr[2] = 3;
+    Console.WriteLine(m);
