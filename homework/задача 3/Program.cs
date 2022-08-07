7b09d548a283c968f6f1f62b12000a578cdc435a
Anton <j10r2@mail.ru>
2022-08-07 19:37:18 +0300

    проверка на чётность

---
+++ homework/задача 3/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Введите любое число");
+try
+{
+    double a = double.Parse(Console.ReadLine());
+    if (a %2 == 0)
+    Console.WriteLine("Введённое число чётное");
+    else
+    {
+        Console.WriteLine("Введённое число нечётное");
+    }
+}
+catch (System.Exception)
+{
+    Console.WriteLine("Это было не число (-_-). Попробуй ещё раз.");
+    try
+    {
+       double a = double.Parse(Console.ReadLine());
+    if (a %2 == 0)
+    Console.WriteLine("Введённое число чётное");
+    else
+    {
+        Console.WriteLine("Введённое число нечётное");
+    }
+    }
+    catch (System.Exception)
+    {
+        Console.WriteLine("Попытки закончились.");   
+    }
+    
+}
+
