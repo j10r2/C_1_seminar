4b5cb1d364545b90b19ecd95c82aa203141975f6
Anton <j10r2@mail.ru>
2022-08-07 18:29:14 +0300

    a > b

---
+++ homework/задача 1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите первое число");
+double a = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+double b = double.Parse(Console.ReadLine());
+
+Console.WriteLine();
+
+if (a > b)
+{
+    Console.WriteLine(a + " больше, чем " + b);
+}
+else
+{
+    Console.WriteLine(b + " больше, чем " + a);
+}
