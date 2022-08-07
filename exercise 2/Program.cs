0f6bffd65b0a3d6cb964c4a1c1999451ff4dae03
Anton <j10r2@mail.ru>
2022-08-07 14:32:42 +0300

    square check

---
+++ exercise 2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("введите первое число");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("введите второе число");
+double b = Convert.ToDouble(Console.ReadLine());
+if (a == b * b)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
