ecdc49f343caf1dd9cefbf6ddbdf69dd644ef15f
Anton <j10r2@mail.ru>
2022-08-07 15:09:19 +0300

    experimental

---
+++ exercise 3/Program.cs
@@ -0,0 +1,17 @@
+﻿string 1 = "Понедельник";
+string 2 = "Вторник";
+string 3 = "Среда";
+string 4 = "Четверг";
+string 5 = "Пятница";
+string 6 = "Суббота";
+string 7 = "Воскресенье";
+Console.WriteLine("Введите число от 1 до 7");
+string day = Console.ReadLine();
+try
+{
+    Console.WriteLine(day);
+}
+catch (ArgumentException ex)
+{
+    Console.WriteLine("Ну и чё ты ввёл???");
+}
